332eeb1583b591db47ee818b7650555ac7610719
muflihnurfaizi <nurfaizimuflih10@gmail.com>
2021-12-23 11:38:31 +0700

    fungsi tabel alhamdulillah

--- public/scripts/sejarahKerjaan.js
+++ public/scripts/sejarahKerjaan.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
+  // hide unhide form input
   $("#tulis-sejarah").on("click", function () {
     $("#form-input").slideToggle("500");
   });
 
+  // animasi label tanggal
   $("#input-tanggal").on("focus", function () {
     $("#label-tanggal").removeClass("opacity-0").addClass("opacity-100");
   });
-
   $("#input-tanggal").on("focusout", function () {
     var str = $("#input-tanggal").val();
     if (str == null || str == "") {
@@ -17,4 +18,25 @@ $(document).ready(function () {
       $("#label-tanggal").removeClass("top-1").addClass("-top-6");
     }
   });
+
+  // tables-------------------------------
+  const tableKerjaan = $("#tableKerjaan").DataTable({
+    dom: 't<"table_Bottom"ip>',
+    buttons: true,
+    pagingType: "simple",
+    pageLength: 10,
+    language: {
+      zeroRecords: "Gak ada datanya boy",
+      paginate: {
+        next: "-->",
+        previous: "<--",
+      },
+    },
+  });
+  //--search box
+  $("#searchBoxTable").on("keyup", function () {
+    tableKerjaan.search(this.value).draw();
+  });
+
+  tableKerjaan.buttons().container().appendTo("#exportButton");
 });
